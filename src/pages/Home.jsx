b30abd43e85040f578084068f1f4d950532a372d
Christine Wong <jumiryoo@gmail.com>
2023-11-20 22:04:06 -0800

    changed the 3D model and added emailjs for the contact page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,94 @@
-import {useState, Suspense} from 'react'
-import { Canvas } from '@react-three/fiber'
-import Loader from '../components/Loader'
-import LittlestTokyo from '../models/littlest_tokyo'
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { Island } from "../models/Island";
+import Loader from "../components/Loader";
+import Sky from "../models/sky";
+import Plane from "../models/Plane";
+import HomeInfo from "../components/HomeInfo";
+
 
 const Home = () => {
-    const [isRotating, setIsRotating] = useState(false);
+ 
+  const [currentStage, setCurrentStage] = useState(1);
+  const [isRotating, setIsRotating] = useState(false);
 
-    const adjustTokyoForScreenSize = () => {
-        let screenScale = null;
-        let screenPosition = [0, -6.5, -43];
-        let rotation = [0,1, 4.7, 0];
+  const adjustIslandForScreenSize = () => {
+    let screenScale, screenPosition;
 
-        if(window.innerWidth < 768) {
-            screenScale = [0.9, 0.9, 0.9];
-            screenPosition = [0, -6.5, -43];
-        } else {
-            screenScale = [0.075, 0.075, 0.075];
-            screenPosition = [0, -6.5, -43];
-        }
-        return [screenScale, screenPosition, rotation]
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43.4];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
     }
 
-    const [tokyoScale, tokyoPosition, tokyoRotation] = adjustTokyoForScreenSize();
+    return [screenScale, screenPosition];
+  };
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.2];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
-   <section className='w-full h-screen relative'>
-    {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        POPUP
-    </div> */}
-    <Canvas 
-        className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
-        camera={{ near: 0.1, far:1000 }}
-    >
+    <section className='w-full h-screen relative'>
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        camera={{ near: 0.1, far: 1000 }}
+      >
         <Suspense fallback={<Loader />}>
-            <directionalLight position={[1,1,3]} intensity={2}/>
-            <ambientLight intensity={0.5}/>
-            <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
-            <LittlestTokyo 
-            position={tokyoPosition}
-            scale={tokyoScale}
-            rotation={tokyoRotation}
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 5, 10]} intensity={2} />
+          <spotLight
+            position={[0, 50, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+          <hemisphereLight
+            skyColor='#b1e1ff'
+            groundColor='#000000'
+            intensity={1}
+          />
+          <Sky 
+          isRotating={isRotating}
+          />
+          <Island
             isRotating={isRotating}
             setIsRotating={setIsRotating}
-            />
+            setCurrentStage={setCurrentStage}
+            position={islandPosition}
+            rotation={[0.1, 4.7077, 0]}
+            scale={islandScale}
+          />
+          <Plane 
+          planeScale ={planeScale}
+          planePosition={planePosition}
+          isRotating={isRotating}
+          rotation={[0, 20, 0]}
+          />
         </Suspense>
-    </Canvas>
-   </section>
-  )
-}
+      </Canvas>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
