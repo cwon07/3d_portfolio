8644e224c728a9146bd323bc162ec787018a7375
Christine Wong <jumiryoo@gmail.com>
2023-11-21 02:15:39 -0800

    error fixing

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 
-import islandScene from "../assets/3d/island.glb";
+import islandScene from "../assets/3D/island.glb";
 
 export function Island({
   isRotating,
